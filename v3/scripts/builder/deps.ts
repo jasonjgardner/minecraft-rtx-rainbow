d0d9aae0167e338cd94647bd88b37a59c889b062
Jason Gardner <im@jasongardner.co>
2024-02-10 08:59:38 -0600

    feat: World template

--- v3/scripts/builder/deps.ts
+++ v3/scripts/builder/deps.ts
@@ -1,4 +1,8 @@
-export { basename, join } from "https://deno.land/std@0.212.0/path/mod.ts";
+export {
+  basename,
+  extname,
+  join,
+} from "https://deno.land/std@0.212.0/path/mod.ts";
 export { copy, ensureDir } from "https://deno.land/std@0.212.0/fs/mod.ts";
 export { hex2rgb } from "https://crux.land/api/get/3RdawE.ts";
 export {
